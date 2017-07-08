c2ab0b1ff56b981464bb561b8cde6e25b84c0cdc
robertsben <roberts.ben@me.com>
2017-07-08 20:35:34 +0100

    Solve problem 2

---
+++ 2_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func testProblem2(t *testing.T) {
+	actual := sol.Problem2(4000000)
+	expected := 4613732
+	if actual != expected {
+		t.Error("For", 4000000, "expected", expected, "got", actual)
+	}
+}
