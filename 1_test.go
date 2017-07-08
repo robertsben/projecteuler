ac10089a7be65d6382ae81cae49e0ed00433770a
robertsben <roberts.ben@me.com>
2017-07-08 20:18:35 +0100

    Add a test

---
+++ 1_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func testProblem1(t *testing.T) {
+	actual := sol.Problem1(3, 5, 1000)
+	expected := 233168
+	if actual != expected {
+		t.Error("For", 3, 5, 1000, "expected", expected, "got", actual)
+	}
+}
